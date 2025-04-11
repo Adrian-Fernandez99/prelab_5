ccaf2785450ddba8a0a05d322783a4d16e1fe380
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-04-11 12:18:56 -0600

    Inicio
    Se usa el postlab4 como plantilla

---
+++ prelab_5/prelab_5/main.c
@@ -0,0 +1,18 @@
+/*
+ * prelab_5.c
+ *
+ * Created: 4/11/2025 11:59:40 AM
+ * Author : adria
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
